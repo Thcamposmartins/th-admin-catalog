280adc0f7e393fab32f68d3789a55c4955efa54b
Thcamposmartins <thcampos_@live.com>
2025-03-25 16:17:23 -0300

    First commit!         (\(        ( -.-)         o_(")(")

---
+++ src/main/java/com/fullcycle/th_admin_catalog/ThAdminCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.th_admin_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThAdminCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThAdminCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullcycle/th_admin_catalog/ThAdminCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcycle.th_admin_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThAdminCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
